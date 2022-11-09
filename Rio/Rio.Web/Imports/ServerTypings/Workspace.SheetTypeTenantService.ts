9bf9323ce02f739c0f200f40571ab59560b4671d
Pravin <pbhandare@antargyan.com>
2022-11-09 16:11:03 +0530

    DeleteButton Added in SheetTypeTenant.

--- Rio/Rio.Web/Imports/ServerTypings/Workspace.SheetTypeTenantService.ts
+++ Rio/Rio.Web/Imports/ServerTypings/Workspace.SheetTypeTenantService.ts
@@ -7,13 +7,15 @@
         export declare function Delete(request: Serenity.DeleteRequest, onSuccess?: (response: Serenity.DeleteResponse) => void, opt?: Q.ServiceOptions<any>): JQueryXHR;
         export declare function Retrieve(request: Serenity.RetrieveRequest, onSuccess?: (response: Serenity.RetrieveResponse<SheetTypeTenantRow>) => void, opt?: Q.ServiceOptions<any>): JQueryXHR;
         export declare function List(request: Serenity.ListRequest, onSuccess?: (response: Serenity.ListResponse<SheetTypeTenantRow>) => void, opt?: Q.ServiceOptions<any>): JQueryXHR;
+        export declare function DeleteSheetTypeTenant(request: string[], onSuccess?: (response: Serenity.SaveResponse) => void, opt?: Q.ServiceOptions<any>): JQueryXHR;
 
         export declare const enum Methods {
             Create = "Workspace/SheetTypeTenant/Create",
             Update = "Workspace/SheetTypeTenant/Update",
             Delete = "Workspace/SheetTypeTenant/Delete",
             Retrieve = "Workspace/SheetTypeTenant/Retrieve",
-            List = "Workspace/SheetTypeTenant/List"
+            List = "Workspace/SheetTypeTenant/List",
+            DeleteSheetTypeTenant = "Workspace/SheetTypeTenant/DeleteSheetTypeTenant"
         }
 
         [
@@ -21,7 +23,8 @@
             'Update', 
             'Delete', 
             'Retrieve', 
-            'List'
+            'List', 
+            'DeleteSheetTypeTenant'
         ].forEach(x => {
             (<any>SheetTypeTenantService)[x] = function (r, s, o) {
                 return Q.serviceRequest(baseUrl + '/' + x, r, s, o);
