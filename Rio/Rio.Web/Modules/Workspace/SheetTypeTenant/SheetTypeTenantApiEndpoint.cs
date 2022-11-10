3f135d2640687a4de5118477ecd2b18a8860ec39
Sameer Dubey <sam@antargyan.com>
2022-11-10 12:27:52 +0530

    new api introduced

--- Rio/Rio.Web/Modules/Workspace/SheetTypeTenant/SheetTypeTenantApiEndpoint.cs
+++ Rio/Rio.Web/Modules/Workspace/SheetTypeTenant/SheetTypeTenantApiEndpoint.cs
@@ -14,30 +14,9 @@ namespace Rio.Workspace.Endpoints
 {
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("Api/Services/Workspace/SheetTypeTenant/[action]")]
-    [ConnectionKey(typeof(MyRow)), ServiceAuthorize(typeof(MyRow))]
+    [ConnectionKey(typeof(MyRow))]//, ServiceAuthorize(typeof(MyRow))]
     public class SheetTypeTenantApiController : ServiceEndpoint
     {
-        [HttpPost, AuthorizeCreate(typeof(MyRow))]
-        public SaveResponse Create(IUnitOfWork uow, SaveRequest<MyRow> request,
-            [FromServices] ISheetTypeTenantSaveHandler handler)
-        {
-            return handler.Create(uow, request);
-        }
-
-        [HttpPost, AuthorizeUpdate(typeof(MyRow))]
-        public SaveResponse Update(IUnitOfWork uow, SaveRequest<MyRow> request,
-            [FromServices] ISheetTypeTenantSaveHandler handler)
-        {
-            return handler.Update(uow, request);
-        }
- 
-        [HttpPost, AuthorizeDelete(typeof(MyRow))]
-        public DeleteResponse Delete(IUnitOfWork uow, DeleteRequest request,
-            [FromServices] ISheetTypeTenantDeleteHandler handler)
-        {
-            return handler.Delete(uow, request);
-        }
-
         [HttpPost]
         public RetrieveResponse<MyRow> Retrieve(IDbConnection connection, RetrieveRequest request,
             [FromServices] ISheetTypeTenantRetrieveHandler handler)
@@ -45,33 +24,11 @@ namespace Rio.Workspace.Endpoints
             return handler.Retrieve(connection, request);
         }
 
-        [HttpPost, AuthorizeList(typeof(MyRow))]
+        [HttpPost, AuthorizeList(typeof(MyRow)), IgnoreAntiforgeryToken]
         public ListResponse<MyRow> List(IDbConnection connection, ListRequest request,
             [FromServices] ISheetTypeTenantListHandler handler)
         {
             return handler.List(connection, request);
-        }
-
-        [AuthorizeList(typeof(MyRow))]
-        public FileContentResult ListExcel(IDbConnection connection, ListRequest request,
-            [FromServices] ISheetTypeTenantListHandler handler,
-            [FromServices] IExcelExporter exporter)
-        {
-            var data = List(connection, request, handler).Entities;
-            var bytes = exporter.Export(data, typeof(Columns.SheetTypeTenantColumns), request.ExportColumns);
-            return ExcelContentResult.Create(bytes, "SheetTypeTenantList_" +
-                DateTime.Now.ToString("yyyyMMdd_HHmmss", CultureInfo.InvariantCulture) + ".xlsx");
-        }
-        [AuthorizeUpdate(typeof(MyRow))]
-        public SaveResponse DeleteSheetTypeTenant(string[] ids, IUnitOfWork uow, [FromServices] ISheetTypeTenantDeleteHandler handler)
-        {
-            foreach (var id in ids)
-            {
-                DeleteRequest deleteRequest = new DeleteRequest();
-                deleteRequest.EntityId = id;
-                DeleteResponse del = handler.Delete(uow, deleteRequest);
-            }
-            return new SaveResponse();
         }
     }
 }
