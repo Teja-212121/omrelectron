4a88d0dcf5c43a398cd36a4eb89584c0a37cd1e7
Firdaus Shaikh <fshaikh@antargyan.com>
2022-11-30 15:44:37 +0530

    ExamId and TenantId added in ImportedScannedBatch Import Dialog.

--- Rio/Rio.Web/Modules/Workspace/ScannedBatch/ScannedBatchGrid.ts
+++ Rio/Rio.Web/Modules/Workspace/ScannedBatch/ScannedBatchGrid.ts
@@ -1,4 +1,5 @@
-﻿import { Decorators, EntityGrid } from '@serenity-is/corelib';
+import { Decorators, EntityGrid, GridRowSelectionMixin } from '@serenity-is/corelib';
+import { ExcelExportHelper } from '@serenity-is/extensions';
 import { ScannedBatchColumns, ScannedBatchRow, ScannedBatchService } from '../../ServerTypes/Workspace';
 import { ScannedBatchDialog } from './ScannedBatchDialog';
 
@@ -11,7 +12,33 @@ export class ScannedBatchGrid extends EntityGrid<ScannedBatchRow, any> {
     protected getLocalTextPrefix() { return ScannedBatchRow.localTextPrefix; }
     protected getService() { return ScannedBatchService.baseUrl; }
 
+    private rowSelection: GridRowSelectionMixin;
+
     constructor(container: JQuery) {
         super(container);
+        this.rowSelection = new GridRowSelectionMixin(this);
+    }
+    protected getColumns() {
+        var columns = super.getColumns();
+        columns.splice(0, 0, GridRowSelectionMixin.createSelectColumn(() => this.rowSelection));
+        return columns;
+    }
+    get selectedItems() {
+        return this.rowSelection.getSelectedKeys().map(x => this.view.getItemById(x));
+    }
+
+    protected getButtons() {
+        var buttons = super.getButtons();
+        buttons.splice(1, 1);
+
+        buttons.push(ExcelExportHelper.createToolButton({
+            grid: this,
+            title: 'Export',
+            service: ScannedBatchService.baseUrl + '/ListExcel',
+            onViewSubmit: () => this.onViewSubmit(),
+            separator: true
+        }));
+
+        return buttons;
     }
 }
