7515e5d081d7ac8ddd8830c21b57a904e84a121a
Firdaus Shaikh <fshaikh@antargyan.com>
2022-12-06 19:37:23 +0530

    Export excel added in Theory Exam Question and Result.

--- Rio/Rio.Web/Modules/Workspace/TheoryExamQuestions/TheoryExamQuestionsGrid.ts
+++ Rio/Rio.Web/Modules/Workspace/TheoryExamQuestions/TheoryExamQuestionsGrid.ts
@@ -1,4 +1,5 @@
-﻿import { Decorators, EntityGrid } from '@serenity-is/corelib';
+import { Decorators, EntityGrid, GridRowSelectionMixin } from '@serenity-is/corelib';
+import { ExcelExportHelper } from '@serenity-is/extensions';
 import { TheoryExamQuestionsColumns, TheoryExamQuestionsRow, TheoryExamQuestionsService } from '../../ServerTypes/Workspace';
 import { TheoryExamQuestionsDialog } from './TheoryExamQuestionsDialog';
 
@@ -11,7 +12,34 @@ export class TheoryExamQuestionsGrid extends EntityGrid<TheoryExamQuestionsRow, 
     protected getLocalTextPrefix() { return TheoryExamQuestionsRow.localTextPrefix; }
     protected getService() { return TheoryExamQuestionsService.baseUrl; }
 
+    private rowSelection: GridRowSelectionMixin;
+
     constructor(container: JQuery) {
         super(container);
+        this.rowSelection = new GridRowSelectionMixin(this);
+    }
+    protected getColumns() {
+        var columns = super.getColumns();
+        columns.splice(0, 0, GridRowSelectionMixin.createSelectColumn(() => this.rowSelection));
+
+        return columns;
+    }
+    get selectedItems() {
+        return this.rowSelection.getSelectedKeys().map(x => this.view.getItemById(x));
+    }
+
+    protected getButtons() {
+        var buttons = super.getButtons();
+        buttons.splice(1, 1);
+
+        buttons.push(ExcelExportHelper.createToolButton({
+            grid: this,
+            title: 'Export',
+            service: TheoryExamQuestionsService.baseUrl + '/ListExcel',
+            onViewSubmit: () => this.onViewSubmit(),
+            separator: true
+        }));
+
+        return buttons;
     }
 }
