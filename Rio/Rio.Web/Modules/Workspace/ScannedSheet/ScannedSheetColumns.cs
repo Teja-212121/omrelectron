78aad333a5c6eecbccb7dabcc34a9593303d3cc6
Dev Serene <dev@antargyan.com>
2022-12-14 15:10:15 +0530

    column updated

--- Rio/Rio.Web/Modules/Workspace/ScannedSheet/ScannedSheetColumns.cs
+++ Rio/Rio.Web/Modules/Workspace/ScannedSheet/ScannedSheetColumns.cs
@@ -35,7 +35,7 @@ namespace Rio.Workspace.Columns
         [DisplayName("Date")]
         public DateTime ScannedBatchInsertDate { get; set; }
         [Width(120), LookupEditor("Workspace.ScannedBatchAsPerDate")]
-        [QuickFilter,/*FilteringOption("cascadeFrom", "ScannedBatchInsertDate")*/]
+        [QuickFilter, FilteringOption("cascadeFrom", "ScannedBatchInsertDate")]
         public Guid ScannedBatchId { get; set; }
         public string ScannedBatchName { get; set; }
         public int TenantId { get; set; }
